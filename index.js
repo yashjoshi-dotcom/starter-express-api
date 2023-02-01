02b147f0cb7c1c0d019f5058550370a2eaa9b68a
Yash Joshi <77540753+yashjoshi-dotcom@users.noreply.github.com>
2023-02-01 23:11:48 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+// const express = require('express')
+// const app = express()
+// app.all('/', (req, res) => {
+//     console.log("Just got a request!")
+//     res.send('Yo!')
+// })
+// app.listen(process.env.PORT || 3000)
+
+
+const http = require('http');
+const port = process.env.PORT || 3000;
+const fs =require("fs");
+const server = http.createServer(function(req, res) 
+{ 
+  // for 10 seconds
+var waitTill = new Date(new Date().getTime() + 10000); 
+while(waitTill > new Date()){}
+
+res.writeHead(200, {'Content-type' : 'text/css'});
+var fileContents = fs.readFileSync('./style.css', {encoding: 'utf8'});
+res.write(fileContents);
+res.end();
 })
-app.listen(process.env.PORT || 3000)
+
+server.listen(port, function(error) {
+if (error) {
+console.log('Something went wrong', error)
+} else {
+    console.log('Server is listening on port ' + port)
+}
+})
